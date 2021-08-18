4c22f866ac4d8e1d836e56a534b36c1dd9ba9ff0
Chawalit Sengsang <chawalit1987@gmail.com>
2021-08-19 00:12:14 +0700

    Master commit: litigant Datetime: 08/19/21 00:12:14

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ssh -i ~/host-key root@139.59.232.82 << EOF > result
   echo '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' | docker login ghcr.io -u midnighttime-cha --password-stdin
+  docker pull ghcr.io/dtdevteam/coj-migration-api:latest
   docker stop coj-migrate-api && docker rm coj-migrate-api
   docker run -d -p 3001:3000 --restart=always -v ~/log:/app/_logfile --name coj-migrate-api ghcr.io/dtdevteam/coj-migration-api:latest
 EOF
