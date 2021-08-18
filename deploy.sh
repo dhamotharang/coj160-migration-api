d4f8581a27c167323b4548a3543646ee86262434
Chawalit Sengsang <chawalit1987@gmail.com>
2021-08-18 13:40:11 +0700

    Master commit: edit Datetime: 08/18/21 13:40:11

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh -i ~/host-key root@139.59.232.82 << EOF > result
-  echo 'cc03d5c7759df0740ff85ffc1dd7410081da10ae' | docker login ghcr.io -u midnighttime-cha --password-stdin
+  echo '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' | docker login ghcr.io -u midnighttime-cha --password-stdin
   docker stop coj-migrate-api && docker rm coj-migrate-api
   docker run -d -p 3001:3000 --restart=always -v ~/log:/app/_logfile --name coj-migrate-api ghcr.io/dtdevteam/coj-migration-api:latest
 EOF
