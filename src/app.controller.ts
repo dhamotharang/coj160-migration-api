99f04c2e2de0c7936008bb3a33344d2f9a08cdb6
Chawalit Sengsang <chawalit1987@gmail.com>
2021-08-30 00:54:18 +0700

    Master commit: authentication Datetime: 08/30/21 00:54:18

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body, Res } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { PostgresUserAuthDTO } from './common/person/dto/user.dto';
+import { ResponseDataController } from './shared/response/response-data.controller';
 
 @ApiTags("Authentication & Access")
 @Controller()
 export class AppController {
+  constructor(
+    private resdata: ResponseDataController
+  ) { }
 
   @UseGuards(LocalAuthGuard)
   @ApiOperation({ summary: "เข้าสู่ระบบ" })
   @Post('auth/login')
-  async login(@Request() req, @Body() body: PostgresUserAuthDTO) {
-    return req.user;
+  async login(@Request() req, @Body() body: PostgresUserAuthDTO, @Res() res) {
+    return this.resdata.responseAuthSuccess(req, res, req.user);
   }
 }
